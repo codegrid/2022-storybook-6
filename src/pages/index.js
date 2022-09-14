1a6b8ce6e685088198561f48d9077b8c049b0700
tomof <fffjt007@gmail.com>
2022-09-14 20:14:12 +0900

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { Button } from '../components/Button'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Storybook Sample</title>
+        <meta name="description" content="Storybook Sample" />
+      </Head>
+      <main className={styles.main}>
+        <form className={styles.form}>
+          <span className={styles.title}>My App</span>
+          <div className={styles.controlGroup}>
+            <label htmlFor="email" className={styles.label}>
+              メールアドレス
+            </label>
+            <input id="email" type="text" className={styles.formControl} />
+          </div>
+          <div className={styles.controlGroup}>
+            <label htmlFor="password" className={styles.label}>
+              パスワード
+            </label>
+            <input id="password" type="password" className={styles.formControl} />
+          </div>
+          <div className={styles.buttonGroup}>
+            <Button type="submit" fullWidth>
+              ログイン
+            </Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  )
+}
